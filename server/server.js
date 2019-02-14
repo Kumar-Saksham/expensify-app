43fcb447de9048ab7916c585d067152c5b9542d1
Saksham Kumar <sakshamkumar@Sam.local>
2019-02-14 21:23:04 +0530

    Setup for heroku

--- server/server.js
+++ server/server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../public')));
-
-const PORT = 3000;
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(path.join(__dirname, '../public/index.html')));
 });
 
-app.listen(PORT, () => {
-    console.log('listening on port', PORT);
+app.listen(port, () => {
+    console.log('listening on port', port);
 })
